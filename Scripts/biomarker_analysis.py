47b6a15608109fb92f1b9b496b351b41f78fa7b0
godabharath70-sketch <godabharath2004@gmail.com>
2026-04-01 17:51:13 +0530

    Initial commit

--- Scripts/biomarker_analysis.py
+++ Scripts/biomarker_analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os 
 import numpy as  np
-from scipy.stats import ttest_ind
+from scipy.stats import test_ind
 from statsmodels.stats.multitest import multipletests
 data = pd.read_csv("../data/gene_expression.csv")
 print(data)
@@ -34,9 +34,9 @@ np.random.seed(0)
 data["p_value"] = np.random.uniform(low=0.001,high=1.0, size=len(data))
 data["neg_log10_pval"] = -np.log10(data["p_value"])
 plt.figure(figsize=(8,6))
-plt.scatter(data["log2FoldChange"], data["neg_log10_pavl"],c="grey")
+plt.scatter(data["log2FoldChange"], data["neg_log10_pval"],c="grey")
 plt.axvline(x=-1, color="red", linestyle="--")
-plt.axlvine(x=-1, color="red", linestyle="--")
+plt.axvline(x=1, color="red", linestyle="--")
 plt.axhline(y=np.log10(0.05), color="blue", linestyle="--")
 plt.xlabel("log2(Fold Change)")
 plt.ylabel("-log10(p-value)")
@@ -49,7 +49,7 @@ list_values = []
 for index, rows in data.iterrows():
     healthy =[rows["H1"], rows["H2"], rows["H3"]]
     disease  =[rows["D1"], rows["D2"],rows["D3"]]
-state,d =  ttest_ind(healthy,disease)
+state,d =  test_ind(healthy,disease)
 list_values.append(d)
 data["p_value"] = list_values
 data['adj_p_values'] = multipletests(data['p_value'],method='far_dh')[1]
